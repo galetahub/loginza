b11697115a65ad6dac077cde40a0685c306880ba
Igor Galeta <galeta.igor@gmail.com>
2010-08-19 16:37:50 +0300

    Include action view helper link_to

--- lib/loginza/view_helper.rb
+++ lib/loginza/view_helper.rb
@@ -3,6 +3,7 @@
 module Loginza
   module ViewHelper
     include ActionView::Helpers::AssetTagHelper
+    include ActionView::Helpers::UrlHelper
     
     # Loginza button
     #
@@ -25,7 +26,7 @@ module Loginza
     # Supported providers:
     #   google, yandex, mailruapi, mailru, vkontakte, facebook, twitter, loginza, myopenid, webmoney, rambler, flickr, lastfm, verisign, aol, steam, openid. 
     #
-    def loginza_button_tag(name, callback_url, options = {}, html_options = {}, &block)
+    def loginza_button_tag(*args, &block)
       if block_given?
         callback_url = args.first
         options      = args.second || {}
@@ -33,40 +34,27 @@ module Loginza
         
         concat(loginza_button_tag(capture(&block), callback_url,  options, html_options))
       else
-        name         = args.first
-        callback_url = args.second
-        options      = args.third || {}
-        html_options = args.four
-        
+        name         = args[0]
+        callback_url = args[1]
+        options      = args[2] || {}
+        html_options = args[3]
+                  
         params = { :token_url => ::Rack::Utils.escape(callback_url) }
         
         if options[:providers]
-          params[:providers_set] = options[:providers].map(&:to_s).join(',')
+          params[:providers_set] = options.delete(:providers).map(&:to_s).join(',')
         else
-          params[:provider] = options[:provider]
+          params[:provider] = options.delete(:provider)
         end
         
         query = ::Rack::Utils.build_query(params)
         url = "https://loginza.ru/api/widget?#{query}"
         
-        if html_options
-          html_options = html_options.stringify_keys
-          href = html_options['href']
-          convert_options_to_javascript!(html_options, url)
-          tag_options = tag_options(html_options)
-        else
-          tag_options = nil
-        end
-
-         href_attr = "href=\"#{url}\"" unless href
-         
-         html = Utils.new_safe_buffer
-         html << javascript_include_tag("http://s1.loginza.ru/js/widget.js")
-         html << "<a #{href_attr}#{tag_options}>#{name || url}</a>".html_safe
-         
-         html
+        html = Utils.new_safe_buffer
+        html << javascript_include_tag("http://s1.loginza.ru/js/widget.js")
+        html << link_to(name, url, options, html_options)
+        html
       end
     end
-
   end
 end
